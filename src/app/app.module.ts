99a85a0d8644e86353c6b9e7476c385d889ccb9d
OrxanMusayev <orxanmusayev1102@gmail.com>
2022-11-27 02:06:48 +0400

    project created and pages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NavBarMenuComponent } from './nav-bar-menu/nav-bar-menu.component';
+import { SideBarMenuComponent } from './side-bar-menu/side-bar-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    CheckoutComponent,
+    CatalogComponent,
+    ProductDetailsComponent,
+    NavBarMenuComponent,
+    SideBarMenuComponent
   ],
   imports: [
     BrowserModule,
